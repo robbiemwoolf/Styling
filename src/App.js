09024e777f5a5c5733352101e457b53f0dd2cc68
Robbie Woolf <robbiemwoolf@gmail.com>
2022-06-21 20:56:33 -0700

    first solution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello!</h1>
-      <h4>How are you?</h4>
-      <h5>Today is a nice day!</h5>
+    <div style={{ textAlign: "center"}}>
+      <h1 style={{ fontFamily: "cursive"}}>Hello!</h1>
+      <h4 style={{ fontFamily: "monospace"}}>How are you?</h4>
+      <h5 className="App-weather-message">Today is a nice day!</h5>
     </div>
   );
 }
